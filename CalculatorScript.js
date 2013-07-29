6d2115f3e50cd0908fff36555415ca02fb55586f
Angelica Siena Abaya <siena.abaya@aestrea.com>
2013-07-29 19:30:15 +0800

    delete unecessary variables, code and fields

--- CalculatorScript.js
+++ CalculatorScript.js
@@ -7,33 +7,24 @@ function anOp(i){
 }
 $(document).ready(function(){
 	var num;
-	var prevNum='';
 	var currentNum='';
 	var operator='';
-	var latestInput ='';
 	var lastChar='';
 	var prevInput='';
 	function evaluate(){
 		lastChar = $('#answerBox').text().slice(-1);
 		prevInput = $('#answerBox').text().charAt($('#answerBox').text().length-1);
 		$('#answerBox').text(currentNum+operator)
-		$('#prevNum').text(currentNum);
 			if(anOp(prevInput)&&anOp(lastChar)){
 				prevInput = $('#answerBox').text().charAt($('#answerBox').text().length-1);
-				console.log('same op');
 				$('#answerBox').text($('#answerBox').text().slice(0,$('#answerBox').text().length -2));
 				currentNum = $('#answerBox').text();
 				$('#answerBox').text(currentNum+operator);
 			}else{
 				$('#answerBox').text(currentNum+operator)
 			}
-		prevNum = currentNum;
 		currentNum = $('#answerBox').text();
 		}	
-	$(this).on('click','td', function(){
-		$('#latestInput').text($(this).text());
-		latestInput = $('#latestInput').text();
-	});
 	$(this).on('click','.num',function(){
 		num = $(this).data('input');
 		$('#answerBox').text(currentNum+''+num);
@@ -50,12 +41,9 @@ $(document).ready(function(){
 	});	
 	$(this).on('click', '#ac', function(){
 		$('#answerBox').text('');
-		$('#prevNum').text('-');
 		$('#hiddenOp').text('');
-		$('#latestInput').text('');
 		currentNum='';
 		operator='';
-		prevNum='';
 	});
 	$(this).on('click', '#del',function(){
 		$('#answerBox').text($('#answerBox').text().slice(0,$('#answerBox').text().length -1));
