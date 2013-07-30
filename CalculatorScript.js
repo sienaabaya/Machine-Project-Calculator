9f07e7471a3db7e7ef32608ae08f76f3d93d5b4b
Angelica Siena Abaya <siena.abaya@aestrea.com>
2013-07-30 10:49:17 +0800

    add exception handler

--- CalculatorScript.js
+++ CalculatorScript.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 	var lastChar='';
 	var prevInput='';
 	function evaluate(){
+	
 		lastChar = $('#answerBox').text().slice(-1);
 		prevInput = $('#answerBox').text().charAt($('#answerBox').text().length-1);
 		$('#answerBox').text(currentNum+operator)
@@ -24,11 +25,24 @@ $(document).ready(function(){
 				$('#answerBox').text(currentNum+operator)
 			}
 		currentNum = $('#answerBox').text();
+	}
+	function enterNumber(){
+			$('#answerBox').text(currentNum+''+num);
+			currentNum= $('#answerBox').text();
+			prevInput = $('#answerBox').text().charAt($('#answerBox').text().length-2);
+			if((anOp(prevInput) || prevInput=='') && num=='0'){
+				$('#answerBox').text($('#answerBox').text().slice(0,$('#answerBox').text().length -1));
+				currentNum = $('#answerBox').text();
+			}else if(prevInput=='.' && num=='.'){
+				$('#answerBox').text($('#answerBox').text().slice(0,$('#answerBox').text().length -1));
+				currentNum = $('#answerBox').text();
+			}
 		}	
 	$(this).on('click','.num',function(){
-		num = $(this).data('input');
-		$('#answerBox').text(currentNum+''+num);
-		currentNum= $('#answerBox').text();		
+		
+			num = $(this).data('input');
+			enterNumber();
+
 	});
 	$(this).on('click','.operator',function(){
 		$('#hiddenOp').text($(this).text());
@@ -36,8 +50,15 @@ $(document).ready(function(){
 		evaluate();
 	});	
 	$(this).on('click','#answer', function(){
-		$('#answerBox').text(eval($('#answerBox').text()));
-		currentNum=$('#answerBox').text();
+		try{
+			$('#answerBox').text(eval($('#answerBox').text()));
+			currentNum=$('#answerBox').text();
+		}catch(err){
+			console.log(err);
+			$('#answerBox').text('');
+			currentNum = $('#answerBox').text('');
+			$('#ac').click();
+		}
 	});	
 	$(this).on('click', '#ac', function(){
 		$('#answerBox').text('');
@@ -50,12 +71,17 @@ $(document).ready(function(){
 		currentNum = $('#answerBox').text();
 	});
 	$('html').keypress(function(e){
-		if(e.keyCode<58 && e.keyCode>47){
-			$('#answerBox').text($('#answerBox').text()+String.fromCharCode(e.keyCode));
-			currentNum=$('#answerBox').text();
+		if(e.keyCode<58 && e.keyCode>45){
+			try{
+				num=String.fromCharCode(e.keyCode);
+				enterNumber();
+			}catch(err){
+				console.log(err);
+				$('#ac').click();
+			}
 		}else if(e.keyCode == 61 || e.keyCode == 13){
 			$('#answer').click();
-		}else if((e.keyCode<48 && e.keyCode>41 || e.keyCode == 37)&&e.keyCode != 44){
+		}else if((e.keyCode<48 && e.keyCode>41 || e.keyCode == 37)&& e.keyCode != 44 || e.keyCode != 46){
 			operator =String.fromCharCode(e.keyCode);
 			$('#hiddenOp').text(String.fromCharCode(e.keyCode));
 			evaluate();
@@ -65,5 +91,6 @@ $(document).ready(function(){
 	$('html').bind('keydown',function(e){
 		if(e.keyCode == 8)
 			$('#del').click();
+
 	});
 });
